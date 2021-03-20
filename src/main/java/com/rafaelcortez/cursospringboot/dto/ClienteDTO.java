91e8fcc4e6a36262b301a6c2840a97c4526e4f38
Rafael Cortez <rafaelcortezp@gmail.com>
2021-03-20 15:45:43 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/rafaelcortez/cursospringboot/dto/ClienteDTO.java
+++ src/main/java/com/rafaelcortez/cursospringboot/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.rafaelcortez.cursospringboot.domain.Cliente;
+import com.rafaelcortez.cursospringboot.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
