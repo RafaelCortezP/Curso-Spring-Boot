8d058f0c10a4b25e189b46d3215cdaacec431d86
Rafael Cortez <rafaelcortezp@gmail.com>
2021-03-21 16:41:38 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/rafaelcortez/cursospringboot/repositories/ProdutoRepository.java
+++ src/main/java/com/rafaelcortez/cursospringboot/repositories/ProdutoRepository.java
@@ -1,11 +1,27 @@
 package com.rafaelcortez.cursospringboot.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rafaelcortez.cursospringboot.domain.Categoria;
 import com.rafaelcortez.cursospringboot.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	
+	
+	//Segunda Opção
+	/*@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);*/
 
 }
---
+++ src/main/java/com/rafaelcortez/cursospringboot/services/ProdutoService.java
@@ -0,0 +1,46 @@
+package com.rafaelcortez.cursospringboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.rafaelcortez.cursospringboot.domain.Categoria;
+import com.rafaelcortez.cursospringboot.domain.Produto;
+import com.rafaelcortez.cursospringboot.repositories.CategoriaRepository;
+import com.rafaelcortez.cursospringboot.repositories.ProdutoRepository;
+import com.rafaelcortez.cursospringboot.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id){
+		
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException( 
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+		
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+		
+	}
+
+}
