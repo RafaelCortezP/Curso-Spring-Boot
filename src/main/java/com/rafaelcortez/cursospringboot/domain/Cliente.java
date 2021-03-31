e22d07f9463dbf629b8a6370874e2670a6dcd869
Rafael Cortez <rafaelcortezp@gmail.com>
2021-03-31 19:03:57 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/rafaelcortez/cursospringboot/domain/Cliente.java
+++ src/main/java/com/rafaelcortez/cursospringboot/domain/Cliente.java
@@ -50,6 +50,8 @@ public class Cliente implements Serializable {
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
+	
+	private String imageUrl;
 
 	public Cliente() {
 		super();
@@ -170,6 +172,14 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageurl) {
+		this.imageUrl = imageurl;
 	}
 	
 }
