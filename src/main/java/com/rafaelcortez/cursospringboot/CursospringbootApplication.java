8b62fef137a9e2806792da8365257a746d7bf8c7
Rafael Cortez <rafaelcortezp@gmail.com>
2021-03-31 17:45:02 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/rafaelcortez/cursospringboot/CursospringbootApplication.java
+++ src/main/java/com/rafaelcortez/cursospringboot/CursospringbootApplication.java
@@ -1,17 +1,25 @@
 package com.rafaelcortez.cursospringboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rafaelcortez.cursospringboot.services.S3Service;
+
 @SpringBootApplication
 public class CursospringbootApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {	
+		
+		s3Service.uploadFile("C:\\Users\\Rafael\\Pictures\\Saved Pictures\\image 1.jpg");
 	}
 }
