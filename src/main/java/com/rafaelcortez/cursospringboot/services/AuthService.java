ecdac7ece6a0ebc69b10c100b31d0a5965c3daa8
Rafael Cortez <rafaelcortezp@gmail.com>
2021-03-30 21:45:34 -0300

    Recuperação de senha

---
+++ src/main/java/com/rafaelcortez/cursospringboot/services/AuthService.java
@@ -0,0 +1,60 @@
+package com.rafaelcortez.cursospringboot.services;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.rafaelcortez.cursospringboot.domain.Cliente;
+import com.rafaelcortez.cursospringboot.repositories.ClienteRepository;
+import com.rafaelcortez.cursospringboot.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class AuthService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EmailService emailService;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	private Random rand = new Random();
+
+	public void sendNewPassword(String email) {
+		
+		Cliente cliente = clienteRepository.findByEmail(email);
+		if(cliente == null) {
+			throw new ObjectNotFoundException("Email não encontrado");
+		}
+		
+		String newPass = newPassword();
+		cliente.setSenha(bCryptPasswordEncoder.encode(newPass));
+		clienteRepository.save(cliente);
+		emailService.sendNewPasswordEmail(cliente, newPass);
+	}
+
+	private String newPassword() {
+		char vet[] = new char[10];
+		for(int i=0; i<10; i++) {
+			vet[i] = randomChar();
+		}
+		return new String(vet);
+	}
+
+	private char randomChar() {
+		int opt = rand.nextInt(3);
+		if(opt == 0) { //gera um digito
+			return (char) (rand.nextInt(10) + 48);
+		}
+		else if(opt == 1) { //gera letra maiuscula
+			return (char) (rand.nextInt(26) + 65);
+		}
+		else { //gera ketra minuscula
+			return (char) (rand.nextInt(26) + 97);
+		}
+	}
+}
--- src/main/java/com/rafaelcortez/cursospringboot/services/EmailService.java
+++ src/main/java/com/rafaelcortez/cursospringboot/services/EmailService.java
@@ -2,6 +2,7 @@ package com.rafaelcortez.cursospringboot.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.rafaelcortez.cursospringboot.domain.Cliente;
 import com.rafaelcortez.cursospringboot.domain.Pedido;
 
 public interface EmailService {
@@ -9,5 +10,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
